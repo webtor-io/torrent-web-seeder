8eaa1f52bff40e958b25917792221135e6a6d034
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-04-09 02:06:18 +0300

    tune params

--- server/services/torrent_client.go
+++ server/services/torrent_client.go
@@ -55,10 +55,10 @@ func (s *TorrentClient) get() (*torrent.Client, error) {
 	cfg.NoUpload = true
 	cfg.DefaultStorage = storage.NewFile(os.TempDir())
 	cfg.DefaultRequestStrategy = torrent.RequestStrategyFuzzing()
-	// cfg.EstablishedConnsPerTorrent = 30
-	// cfg.HalfOpenConnsPerTorrent = 15
-	// cfg.TorrentPeersHighWater = 200
-	// cfg.TorrentPeersLowWater = 100
+	cfg.EstablishedConnsPerTorrent = 100
+	cfg.HalfOpenConnsPerTorrent = 50
+	cfg.TorrentPeersHighWater = 1000
+	cfg.TorrentPeersLowWater = 500
 	if s.rLimit != -1 {
 		cfg.DownloadRateLimiter = rate.NewLimiter(rate.Limit(s.rLimit), int(s.rLimit))
 	}
